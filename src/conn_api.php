c9214904c42ee6ef876286862756caa20ec3bfc5
nicolasloison13 <nicolasloison13@icloud.com>
2023-02-02 12:52:19 +0100

    add html base + add weather and icon in HTML with API + add conn_api file + add .env.exemple for key

---
+++ src/conn_api.php
@@ -0,0 +1,23 @@
+<?php
+require "../vendor/autoload.php";
+
+$api_key = $_ENV['OPEN_WEATHER_API_KEY'];
+$city_name = "Lausanne";
+
+
+$api_url = 'http://api.openweathermap.org/data/2.5/weather?q=' . $city_name . '&appid=' . $api_key;
+
+
+$weather_data = json_decode(file_get_contents($api_url), true);
+
+// temperature et fahrenheit to celsius
+$temperature = $weather_data['main']['temp'];
+$temperature_in_celsius = round($temperature - 273.15);
+
+// main et description
+$temperature_current_weather = $weather_data['weather'][0]['main'];
+$temperature_current_weather_description = $weather_data ['weather'][0]['description'];
+
+// icon
+$temperature_current_weather_icon = $weather_data['weather'][0]['icon'];
+$affichage_icon = "<img src='http://openweathermap.org/img/wn/" . $temperature_current_weather_icon . "@2x.png' />";
