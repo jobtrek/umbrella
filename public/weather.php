9c2bb8e129ca66b83331f386edc25005a542bd10
maudelinDS <maudelin.delescaut@gmail.com>
2023-02-13 18:59:03 +0100

    Add Select random activity,Fix Display activity,ADD Design

--- public/weather.php
+++ public/weather.php
@@ -14,92 +14,55 @@ require "../src/conn_db.php";
 require "./header.php";
 ?>
 
-    <div class="card w-96 bg-base-100 shadow-xl ml-8 mt-10">
+    <div class="grid grid-cols-2">
+        <div class="top-part">
 
-        <p> <?= $affichage_icon ?> </p>
 
-        <div class="card-body">
-            <p><?= $temperature ?> ° </p>
-            <p><?= $temperature_current_name; ?></p>
-            <p> <?= $temperature_current_weather_description; ?> </p>
-            <p> <?= $date; ?> </p>
+            <div class="card w-906 h-50 bg-base-100 shadow-xl ml-8 mt-10  ">
+
+                <p> <?= $affichage_icon ?> </p>
+
+                <div class="card-body">
+                    <p><?= $temperature_in_celsius ?> ° </p>
+                    <p><?= $temperature_current_name; ?></p>
+                    <p> <?= $temperature_current_weather; ?> </p>
+                </div>
+
+                <div class="card-body bg-gradient-to-r from-indigo-200">
+
+                    <p> vitesse du vent <?= $temperature_current_wind ?> </p>
+                    <p> description : <?= $temperature_current_weather_description; ?> </p>
+                    <p> humidity : <?= $temperature_current_main_humidity; ?> </p>
+                    <p> pressure : <?= $temperature_current_main_pressure; ?> </p>
+                    <p> nuages : <?= $temperature_current_clouds; ?>%</p>
+                    <p> temp max : <?= $temperature_max_in_celsius; ?></p>
+                    <p> temp min : <?= $temperature_min_in_celsius; ?></p>
+                    <p> ressentis : <?= $temperature_feels_in_celsius; ?></p>
+                </div>
+            </div>
         </div>
+        <div class="grid grid-cols-4 gap-6 ">
 
-        <div class="card-body bg-gradient-to-r from-indigo-200">
+            <?php foreach ($activity as $activities) { ?>
 
-            <p> vitesse du vent <?= $temperature_current_wind ?> km/h </p>
-            <p> humidity : <?= $temperature_current_main_humidity; ?> % </p>
-            <p> pressure : <?= $temperature_current_main_pressure; ?> </p>
-            <p> nuages : <?= $temperature_current_clouds; ?>%</p>
-            <p> temp max : <?= $temperature_max; ?> ° </p>
-            <p> temp min : <?= $temperature_min; ?> °</p>
-            <p> ressentis : <?= $temperature_feels; ?> ° </p>
+                <div class="card-body ">
+                    <h1 class="flex justify-center"><strong> <?= $activities->name_activity ?></strong></h1>
+                    <h2 class="flex justify-center"> <?= $activities->environ_name ?> </h2>
+                    <div class="card w-80 bg-base-100 shadow-xl hover:bg-secondary/50">
+                        <div class="card-body">
+                            <h2>
+                                activity : <?= $activities->name_activity ?><br>
+                                score : <?= $activities->score ?><br>
+                                environ : <?= $activities->environ_name ?><br>
+
+                            </h2>
+
+                        </div>
+                    </div>
+                </div>
+                <br>
+            <?php } ?>
+
         </div>
-    </div>
-
-    <div>
-        <?php for ($i = 1; $i <= 4; $i++) { ?>
-
-            <div class="card-body ">
-                <div class="card w-96 bg-base-100 shadow-xl ">
-                    <div class="card-body">
-                        <h2>
-                            <p> <?php $temperature_current_weather_icon = $weather_data["list"][$i]['weather'][0]['icon']; ?> </p>
-                            <p> <?= $affichage_icon = "<img src='http://openweathermap.org/img/wn/"
-                                    . $temperature_current_weather_icon . "@2x.png'/>"; ?>  </p>
-                            <p> <?= round($weather_data["list"][$i]['main']['temp']); ?> °</p>
-                            <p> <?= $date = $weather_data["list"][$i]['dt_txt']; ?></p>
-
-                        </h2>
-                    </div>
-                </div>
-            </div>
-
-        <?php } ?>
-    </div>
-
-    <div>
-        <?php for ($i = 8; $i <= 39; $i += 8) { ?>
-
-            <div class="card-body ">
-                <div class="card w-96 bg-base-100 shadow-xl ">
-                    <div class="card-body">
-                        <h2>
-                            <p> <?php $temperature_current_weather_icon = $weather_data["list"][$i]['weather'][0]['icon']; ?> </p>
-                            <p> <?= $affichage_icon = "<img src='http://openweathermap.org/img/wn/"
-                                    . $temperature_current_weather_icon . "@2x.png'/>"; ?>  </p>
-                            <p> <?= round($weather_data["list"][$i]['main']['temp']); ?> °</p>
-                            <p> <?= $date = $weather_data["list"][$i]['dt_txt']; ?></p>
-
-                        </h2>
-                    </div>
-                </div>
-            </div>
-
-        <?php } ?>
-    </div>
-
-    <br><br>
-    <div class="grid grid-cols-4 gap-4">
-
-        <?php foreach ($activity as $activities) { ?>
-
-            <div class="card-body ">
-                <div class="card w-96 bg-base-100 shadow-xl ">
-                    <div class="card-body">
-                        <h2>
-                            id : <?= $activities->activity_id ?> <br>
-                            activity : <?= $activities->name_activity ?><br>
-                            score : <?= $activities->score ?><br>
-                            environ : <?= $activities->environ_name ?><br>
-
-                        </h2>
-
-                    </div>
-                </div>
-            </div>
-            <br>
-        <?php } ?>
-
     </div>
 <?php require "./footer.php" ?>
